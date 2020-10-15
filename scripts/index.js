a98d9ad462d0be70f298a8d0c7c870058cdb61a8
Ramil Ashrafulin <HEHAHA@bk.ru>
2020-10-16 00:28:37 +0600

    feat

--- scripts/index.js
+++ scripts/index.js
@@ -9,4 +9,33 @@ function classActiveAdd() {
     body.classList.toggle('lock');
 }
 
+<<<<<<< HEAD
 burger.addEventListener('click', classActiveAdd);
+
+
+const descriptionContainer = document.querySelector('.description__container');
+const descriptionAdress = Array.from(descriptionContainer.querySelectorAll('.description__adress'));
+const descriptionContacts = descriptionContainer.querySelector('.description__contacts');
+const descriptionSocialLink = descriptionContainer.querySelector('.description__social-link');
+
+function changeDescriptionColor() {
+    descriptionAdress.forEach(function(event) {
+        event.style.color = 'black';
+    })
+    descriptionContacts.style.color = 'black';
+    descriptionSocialLink.style.color = 'black';
+}
+
+function callBackDescriptionColor() {
+    descriptionAdress.forEach(function(event) {
+        event.style.color = 'white';
+    })
+    descriptionContacts.style.color = 'white';
+    descriptionSocialLink.style.color = 'white';
+}
+
+descriptionContainer.addEventListener('mouseover', changeDescriptionColor);
+descriptionContainer.addEventListener('mouseout', callBackDescriptionColor);
+=======
+burger.addEventListener('click', classActiveAdd);
+>>>>>>> 1f2c5b109f7df7687dfde1c80549669d894d6a3f
