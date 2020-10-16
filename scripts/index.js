1eaa0c11948a58ae7c08ee4939053d12599e6c73
xLeeVeNx <berdyev.damil1@mail.ru>
2020-10-16 14:40:20 +0600

    chanded

--- scripts/index.js
+++ scripts/index.js
@@ -17,22 +17,43 @@ const descriptionAdress = Array.from(descriptionContainer.querySelectorAll('.des
 const descriptionContacts = descriptionContainer.querySelector('.description__contacts');
 const descriptionSocialLink = descriptionContainer.querySelector('.description__social-link');
 const page = document.querySelector('.page');
-function changeDescriptionColor() {
+const widthValue = $(window).width();
+
+if( widthValue > 1080 ) {
+    $(document).ready(function(){
+        $(".description__container").hover(function(){
+          $(this).css("background-color", "rgba(255, 255, 255, 0.85)");
+          }, function(){
+          $(this).css("background-color", "");
+        });
+      });
+        
+    function changeDescriptionColor() {
     descriptionAdress.forEach(function(event) {
         event.style.color = 'black';
     })
     descriptionContacts.style.color = 'black';
     descriptionSocialLink.style.color = 'black';
+        
+    }
     
+    function callBackDescriptionColor() {
+        descriptionAdress.forEach(function(event) {
+            event.style.color = 'white';
+        })
+        descriptionContacts.style.color = 'white';
+        descriptionSocialLink.style.color = 'white';
+    }
+    
+    descriptionContainer.addEventListener('mouseover', changeDescriptionColor);
+    descriptionContainer.addEventListener('mouseout', callBackDescriptionColor);
+}
+else {
+    $(document).ready(function(){
+        $(".description__container").hover(function(){
+          $(this).css("background-color", "black");
+          }
+        );
+      });
 }
 
-function callBackDescriptionColor() {
-    descriptionAdress.forEach(function(event) {
-        event.style.color = 'white';
-    })
-    descriptionContacts.style.color = 'white';
-    descriptionSocialLink.style.color = 'white';
-}
-
-descriptionContainer.addEventListener('mouseover', changeDescriptionColor);
-descriptionContainer.addEventListener('mouseout', callBackDescriptionColor);
